852e508e7eeb413b737983ab0b5f8674cc1afb2d
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-10-28 14:19:13 -1000

    Add more steps

--- problem_solving.rb
+++ problem_solving.rb
@@ -1,2 +1,2 @@
-first_step_to_plant_a_tree = "Dig a hole"
-p first_step_to_plant_a_tree.length.even?
+steps_to_plant_a_tree = ["Dig a hole", "plant tree", "water"]
+p steps_to_plant_a_tree.length.even?
