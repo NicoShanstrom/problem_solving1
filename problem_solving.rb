19661417a9314c4173b170ef379cde24d2cfbb55
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-10-28 14:28:57 -1000

    Add current time

--- problem_solving.rb
+++ problem_solving.rb
@@ -1,2 +1,13 @@
 steps_to_plant_a_tree = ["Dig a hole", "plant tree", "water"]
 p steps_to_plant_a_tree.length.even?
+#What do you like about this process?
+    #It is always good to have a method to the madness. Trust the process. With a plan, it is easier to achieve the outcome. Without a plan, I am all over the place. 
+#What might make this process challenging?
+    #It could be more challenging if one does not break down the process into basic pieces, it may be hard to google a long block of information. If you are not specific, you'll get too much information.
+#Are there any steps you would add or modify?
+    # Take a break if you are struggling. Being frustrated and in a rush does not help.
+
+def current_time = 14
+    "Good afternoon"
+end
+current_time
